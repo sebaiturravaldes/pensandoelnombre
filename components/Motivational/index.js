df0755b52402553fff997466edf7381f7902ad07
Sebastián Iturra <iturra.sebastian@hotmail.com>
2021-12-21 23:56:47 -0300

    add more phrases

--- components/Motivational/index.js
+++ components/Motivational/index.js
@@ -1,5 +1,8 @@
 const phrases = [
-  'Si una persona quiere aprender y te pregunta algo, explícaselo, en lugar de hacerla sentir tonta... por que hace tiempo tú tampoco lo sabías.',
+  'Si una persona quiere aprender y te pregunta algo, explícaselo, en lugar de hacerla sentir tonta... por que hace tiempo tú tampoco lo sabías.', //linkedin
+  'No hay seniors sin juniors.', //linkedin
+  '¿Qué debes hacer cuando estas contra la pared?, ¡Romper la pared!.',
+  'Nunca es tarde.',
   'Nos levantamos día tras día a cambiar el mundo, eso es lo que hacemos.'
 ]
 
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Aprende a programar</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="google" content="notranslate" />
       </Head>
