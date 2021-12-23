cfb529f170cbebfdec09e0fa65f60140952e430b
Sebastián Iturra <iturra.sebastian@hotmail.com>
2021-12-23 00:07:23 -0300

    Update index.js

--- components/Motivational/index.js
+++ components/Motivational/index.js
@@ -1,7 +1,8 @@
 const phrases = [
   'Si una persona quiere aprender y te pregunta algo, explícaselo, en lugar de hacerla sentir tonta... por que hace tiempo tú tampoco lo sabías.', //linkedin
   'No hay seniors sin juniors.', //linkedin
-  '¿Qué debes hacer cuando estas contra la pared?, ¡Romper la pared!.',
+  'Yo no tengo sueños, tengo metas, y seguiré a la siguiente.', //SUITS
+  '¿Qué debes hacer cuando estas contra la pared?, ¡Romper la maldita pared!.', //SUITS
   'Nunca es tarde.',
   'Nos levantamos día tras día a cambiar el mundo, eso es lo que hacemos.'
 ]
