becac8d84361f5a7c5519dcd7418981c96f93deb
Sebastián Iturra <iturra.sebastian@hotmail.com>
2021-12-23 20:41:24 -0300

    Update index.js

--- components/Motivational/index.js
+++ components/Motivational/index.js
@@ -3,6 +3,7 @@ const phrases = [
   'No hay seniors sin juniors.', //linkedin
   'Yo no tengo sueños, tengo metas, y seguiré a la siguiente.', //SUITS
   '¿Qué debes hacer cuando estas contra la pared?, ¡Romper la maldita pared!.', //SUITS
+  'A veces las personas que todos piensan que no harán nada, son las que hacen cosas que nadie había imaginado.', // Alan turing
   'Nunca es tarde.',
   'Nos levantamos día tras día a cambiar el mundo, eso es lo que hacemos.'
 ]
